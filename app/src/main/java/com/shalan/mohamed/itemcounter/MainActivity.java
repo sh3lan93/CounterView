66c6d3d7a9493464c8e10c4bbc1bf49a36f299c1
shalan <mohamed.sh3lan_93@hotmail.com>
2017-11-18 21:04:00 +0200

    show library usage

--- app/src/main/java/com/shalan/mohamed/itemcounter/MainActivity.java
+++ app/src/main/java/com/shalan/mohamed/itemcounter/MainActivity.java
@@ -2,12 +2,29 @@ package com.shalan.mohamed.itemcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.shalan.mohamed.itemcounterview.CounterListener;
+import com.shalan.mohamed.itemcounterview.IncDecView;
 
+public class MainActivity extends AppCompatActivity implements CounterListener {
+
+    private IncDecView incDecView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        incDecView = findViewById(R.id.itemCounter);
+        incDecView.setCounterListener(this);
+    }
+
+    @Override
+    public void onIncClick(String value) {
+        Toast.makeText(this, value, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDecClick(String value) {
+        Toast.makeText(this, value, Toast.LENGTH_LONG).show();
     }
 }
