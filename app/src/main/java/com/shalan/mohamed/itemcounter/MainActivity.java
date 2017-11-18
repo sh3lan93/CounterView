aad9fedfe7f2f1c4d83edeeb012b92da765255d3
shalan <mohamed.sh3lan_93@hotmail.com>
2017-11-19 00:38:46 +0200

    add jitpack configuration

--- app/src/main/java/com/shalan/mohamed/itemcounter/MainActivity.java
+++ app/src/main/java/com/shalan/mohamed/itemcounter/MainActivity.java
@@ -4,27 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.shalan.mohamed.itemcounterview.CounterListener;
-import com.shalan.mohamed.itemcounterview.IncDecView;
 
-public class MainActivity extends AppCompatActivity implements CounterListener {
+public class MainActivity extends AppCompatActivity {
 
-    private IncDecView incDecView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        incDecView = findViewById(R.id.itemCounter);
-        incDecView.setCounterListener(this);
-    }
-
-    @Override
-    public void onIncClick(String value) {
-        Toast.makeText(this, value, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onDecClick(String value) {
-        Toast.makeText(this, value, Toast.LENGTH_LONG).show();
     }
 }
